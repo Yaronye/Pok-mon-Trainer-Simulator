00565bdcc8da0501c8e9db182212c49eb290d4e6
Yaronye <ronya_budak@hotmail.com>
2025-06-02 17:08:36 +0200

    Add project files.

---
+++ Pokémon Trainer Simulator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Pokémon_Trainer_Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
